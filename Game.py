1f634a487ddc7c5d369b22898409366c48b19ca4
PAWAN G <140328953+PAWAN-G14@users.noreply.github.com>
2023-08-26 12:41:16 +0530

    Documented Game.py

--- Game.py
+++ Game.py
@@ -7,14 +7,19 @@ class Game:
     # This is a class that . . . 
     # -> Sets up our Board object
     # -> Asks the users role and starts the game
-    # -> Provides a function the enables the user to make make moves
-    # -> Provides a function that enables the computer to make moves
+    # -> Provides a method the enables the user to make make moves
+    # -> Provides a method that enables the computer to make moves
     # -> Updates the status of the game and checks whether the game ended
     # -> Displays the results
     # -> Asks for rematch
     # -> Handles misinputs in all cases. 
 
     def __init__(self):
+
+	# Initializes the Game.
+	# Prompts the user to select a role.
+	# Makes the first move of the user and the computer
+	
         self.board  = Board()
         self.status = 2
         while 1:
@@ -30,6 +35,14 @@ class Game:
         self.computerTurn()    
 
     def userTurn(self):
+
+	# This method enables the user to make moves
+	    
+	# Step 1: Draw the board.
+	# Step 2: Prompt user and get move input.
+	# Step 3: Check whether input is valid. If invalid, the leave a kind message and return to Step 2.
+	# Step 4: After getting the valid input, make the move using Board.makeMove()
+	    
         system("clear")
         self.board.draw()
         while 1:
@@ -39,9 +52,18 @@ class Game:
                 continue
             break
 
-    def computerTurn(self): self.board.makeMove(bestMove(self.board.pos, not self.user), not self.user)
+    def computerTurn(self): 
+	    # Gets the best move using bestMove() function. and makes the move using Board.makeMove()
+	    self.board.makeMove(bestMove(self.board.pos, not self.user), not self.user)
 
     def updateStatus(self): 
+
+	# This method analyses the position, checks if the game ended and updates the results by changing self.status
+	# If game ended then the results are displayed in the CLI.
+	# If, self.status==2 : Game did not end
+	# If, self.status==0 : Tie
+	# If, self.status!=2 : Computer won. (Only computer could win.)
+	
         self.status = winLoseDraw(self.board.pos)
         if   self.status==2: pass
         elif self.status==0:
@@ -54,6 +76,14 @@ class Game:
             print("Computer Wins . . . Better luck next time")
     
     def askRematch(self):
+	    
+	# This method requests the user for a rematch.
+	    
+	# Step 1: Request the user for a rematch.
+	# Step 2: Get user input in the form of y/n.
+	# Step 3: Check whether input is valid. If invalid, the leave a kind message and return to Step 2.
+	# Step 4: After getting the valid input, return the appropriate boolean value i.e true for yes and false for no.
+	    
         user_input = input("Would you like to have a rematch? : ")
         while 1:
             if   user_input[0].lower() == 'y': return 1
@@ -63,5 +93,3 @@ class Game:
                 continue
             break
         
-
-
